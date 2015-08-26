39dee013ef058c8d5b71f24bc7466abbd6ded058
cskiwi <glenn.latomme@gmail.com>
2015-08-26 13:12:22 +0200

    pushpin controller

---
+++ app/controllers/PushPin/PushPin.js
@@ -0,0 +1,9 @@
+/* global define */
+
+define(['app'], function (app) {
+
+    'use strict';
+
+    app.register.controller('PushPinController', function ($scope, $rootScope, $injector) {
+    });
+});
